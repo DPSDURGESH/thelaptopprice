e9354b118cd11e0dea909122cda6045404db3bd2
Durgest <durgest@gmail.com>
2023-03-21 20:29:14 +0530

    Fisrt change

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle
 import numpy as np
 
+
 # import the model
 pipe = pickle.load(open('pipe.pkl','rb'))
 df = pickle.load(open('df.pkl','rb'))
@@ -26,20 +27,22 @@ touchscreen = st.selectbox('Touchscreen',['No','Yes'])
 # IPS
 ips = st.selectbox('IPS',['No','Yes'])
 
-# screen size
+# screen
 screen_size = st.number_input('Screen Size')
 
 # resolution
-resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
+resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900',
+                                               '3840x2160','3200x1800','2880x1800',
+                                               '2560x1600','2560x1440','2304x1440'])
 
-#cpu
-cpu = st.selectbox('CPU',df['Cpu brand'].unique())
+# cpu
+cpu = st.selectbox('Brand',df['Cpu brand'].unique())
 
 hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
 
 ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
 
-gpu = st.selectbox('GPU',df['Gpu brand'].unique())
+gpu = st.selectbox('GUP',df['Gpu brand'].unique())
 
 os = st.selectbox('OS',df['os'].unique())
 
@@ -58,9 +61,9 @@ if st.button('Predict Price'):
 
     X_res = int(resolution.split('x')[0])
     Y_res = int(resolution.split('x')[1])
-    ppi = ((X_res**2) + (Y_res**2))**0.5/screen_size
+    ppi = (((X_res**2) + (Y_res**2))**0.5/screen_size)
     query = np.array([company,type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
 
     query = query.reshape(1,12)
-    st.title("The predicted price of this configuration is " + str(int(np.exp(pipe.predict(query)[0]))))
 
+    st.title("The predicted price of this configuration is" + int(np.exp(pipe.predict(query)[0])))
